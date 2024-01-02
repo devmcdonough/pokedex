aa9bac8db0750d0c316dc7462eb73baed62a5167
devmcdonough <devmcdonough@gmail.com>
2024-01-02 12:31:19 -0500

    Updated pokemonList loops to forEach loops

--- js/scripts.js
+++ js/scripts.js
@@ -22,36 +22,43 @@ let pokemonList = [
   }
 ];
 
-for (let i = 0; i < pokemonList.length; i++)
-if (pokemonList[i].height > .5) {
-  document.write("<p>" + pokemonList[i].name + ": " + pokemonList[i].height + " Wow, that's big!" + "</p>");
-} else {
-  document.write("<p>" + pokemonList[i].name + ": " + pokemonList[i].height) + "</p>";
-}
+let pokemonList2 = [
+  {
+    name: "Pikamurraychu",
+    height: 0.5,
+    type: "Electric",
+    species: "Mouse",
+    abilities: "Static",
+  },
+  {
+    name: "Charmmurrayander",
+    height: 0.6,
+    type: "Fire",
+    species: "Lizard",
+    abilities: "Blaze",
+  },
+  {
+    name: "Weemurraydle",
+    height: 0.4,
+    type: "Bug",
+    species: "Hairy Bug",
+    abilities: "Shield Dust",
+  }
+];
 
 
-  
+pokemonList.forEach(function(pokemon) {
+  if (pokemon.height > .5) {
+  document.write("<p>" + pokemon.name + ": " + pokemon.height + " - Wow, that's big!" + "</p>");
+    } else {
+      document.write("<p>" + pokemon.name + ": " + pokemon.height) + "</p>";
+    }
+})
 
-
-    //let condition = true;
-    //let condition = 1 >= 1;
-    // let condition = 1 >= 2;
-    // let condition = 1 != 2;
-    // let condition = null;
-    // let condition = false && '';
-    // let condition = false && 'Hello World!';
-    // let condition = null || false;
-    // let condition = false || 42;
-    // let condition = (42 >= (2 + 40)) && (false || 'Hello World');
-    //* let condition = true && ' ';
-
-    //if(condition){
-    //console.log('The result is true:', condition)
-    //}else{
-    //console.log('The result is false:', condition)
-    //}
-
-    
-    
-
-
+pokemonList2.forEach(function(pokemon2) {
+  if (pokemon2.height > .5) {
+  document.write("<p>" + pokemon2.name + ": " + pokemon2.height + " - Wow, that's big!" + "</p>");
+    } else {
+      document.write("<p>" + pokemon2.name + ": " + pokemon2.height) + "</p>";
+    }
+})
