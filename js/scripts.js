2d6586c98914f1f430a377ee1b51ea03055de0d3
devmcdonough <devmcdonough@gmail.com>
2024-01-03 19:27:37 -0500

    Added event listener to buttons

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,16 @@ let pokemonRepository = (function () {
       height: 0.5,
       type: "Electric",
       species: "Mouse",
-      abilities: "Static",
+      abilities: ["Static", "Blaze", "Shield Dust"],
+
     },
     {
       name: "Charmander",
       height: 0.6,
-      type: "Fire",
+      type: {
+        name: "Fire",
+        description: "Hot. Ouch!",
+      },
       species: "Lizard",
       abilities: "Blaze",
     },
@@ -24,23 +28,104 @@ let pokemonRepository = (function () {
     }
   ]
 
-  return {
-    add: function (pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function () {
-      return pokemonList;
-    }
+function add(pokemon) {
+  
+}
+
+  function getAll() {
+    return pokemonList;
   }
-})();
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    button.addEventListener('click', function(showDetails) {
+      console.log(showDetails);
+    })
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+    }
+    
+  }
+)();
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Bulbasaur', height: '0.9' })
+pokemonRepository.add({ name: 'Bulbasaur', height: 0.9 })
 
 pokemonRepository.getAll().forEach(function (pokemon) {
-  if (pokemon.height > .5) {
-    document.write("<p>" + pokemon.name + ": " + pokemon.height + " - Wow, that's big!" + "</p>");
-  } else {
-    document.write("<p>" + pokemon.name + ": " + pokemon.height) + "</p>";
-  }
+  pokemonRepository.addListItem(pokemon);
 })
+
+
+
+// let button = document.querySelector('button');
+// button.addEventListener('click', function (event) {
+//   let target = event.target;
+//   target.classList.toggle('button--red');
+//   target.classList.toggle('button--green');
+// });
+
+// let element = document.querySelector('select');
+// element.value; // returns '1' as that’s currently selected
+// element.value = '2';
+
+// let selectedIndex = element.selectedIndex;
+// if (selectedIndex > -1) {
+//   let selectedOption = element.querySelectorAll('option')[selectedIndex];
+// }
+
+// let ageGroupElement = document.querySelector('input[name="age-group"]:checked');
+// let value = ageGroupElement ? ageGroupElement.value : null;
+
+
+
+
+// let foodList = ['tuna', 'cheese', 'salad', 'nutter butter'];
+
+// document.write('<h3>===for LOOP===</h3>');
+// for (let i = 0; i < foodList.length; i++) {
+//   document.write('<p>' + foodList[i] + '</p>');
+// }
+
+// document.write('<h3>===forEach LOOP===</h3>');
+// foodList.forEach(function (foodItem, num) {
+//   document.write('<p>' + (num + 1) + foodItem +  '</p>');
+// });
+
+// let container = document.querySelector('.container');
+// let button = document.createElement('button');
+// button.innerText = 'Click me';
+// container.appendChild(button);
+
+// let container = document.querySelector('.container');
+// let elementToRemove = document.querySelector('p');
+// elementToRemove.parentElement.removeChild(elementToRemove);
+
+// let subtitle = document.querySelector('h3');
+// console.log(subtitle.innerText);
+// subtitle.innerText = "Hey guuuuys";
+// console.log(subtitle.innerText);
+
+// let container = document.querySelector('.container');
+// container.innerHTML = '<button>Click me</button>'
+
+// element = document.querySelector('div');
+// console.log(element.getAttribute('id'));
+// console.log(element.hasAttribute('value'));
+// element.setAttribute('id', 'new-id');
+// console.log(element.getAttribute('id'));
+
+// element.classList.contains('container');
