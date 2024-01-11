afccbc27c39107b37f6822157775cc94a675845a
devmcdonough <devmcdonough@gmail.com>
2024-01-10 19:44:50 -0500

    Exercise 1.8 - Got Images Working

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@ let pokemonRepository = (function () {
 
   let pokemonList = []
 
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   let modalContainer = document.querySelector('#modal-container');
 
 
+  // This adds pokemon to the PokemonList if they are valid
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
@@ -18,12 +19,12 @@ let pokemonRepository = (function () {
     }
   }
 
-
+  // Gets value of array
   function getAll() {
     return pokemonList;
   }
 
-  function showModal(title, text, img) {
+  function showModal(title, text, pokemonImage) {
     //clear existing modal content
     modalContainer.innerHTML = '';
 
@@ -44,14 +45,14 @@ let pokemonRepository = (function () {
     let contentElement = document.createElement('p');
     contentElement.innerText = text;
 
-    let imgContainer = document.querySelector('#image-container');
     let imageElement = document.createElement('img');
-    imageElement.src = img;
+    imageElement.src = pokemonImage;
 
     modal.appendChild(closeButtonElement);
     modal.appendChild(titleElement);
-    imgContainer.appendChild(imageElement);
+    modal.appendChild(imageElement);
     modal.appendChild(contentElement);
+
     modalContainer.appendChild(modal);
 
     modalContainer.classList.add('is-visible');
@@ -155,9 +156,17 @@ let pokemonRepository = (function () {
   }
 
   function showDetails(pokemon) {
+    if (isValidImageUrl(pokemon.imageUrl)) {
+      console.log('Valid image URL', pokemon.imageUrl);
+    }
       showModal(pokemon.name, `Height: ${pokemon.height}`, pokemon.imageUrl);
-    };
+    } 
+    
   
+  function isValidImageUrl(url) {
+    const imageExtensions = /\.(jpg|jpeg|png|gif|mbp|svg)$/i;
+    return imageExtensions.test(url);
+  }
 
   return {
     add: add,
@@ -172,94 +181,9 @@ let pokemonRepository = (function () {
 )();
 
 
-
 pokemonRepository.loadList().then(function () {
   // Now the data is loaded!
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-
-
-    // console.log(pokemonRepository.getAll());
-    // pokemonRepository.add({ name: 'Bulbasaur', height: 0.9 })
-
-    // pokemonRepository.getAll().forEach(function (pokemon) {
-    //   pokemonRepository.addListItem(pokemon);
-    // })
-
-    // let thisIsAJsonString = '{"name":"Lisa","age":27}';
-    // let lisa = JSON.parse(thisIsAJsonString);
-    // console.log(lisa.name, lisa.age);
-
-    // let john = {
-    //   name: 'John',
-    //   age: 31
-    // };
-    // let jsonString = JSON.stringify(john);
-    // console.log(jsonString); // '{"name":"John","age":31}'
-
-    // let list = [1, 2, 3];
-    // console.log(JSON.stringify(list)); // '[1,2,3]'
-    // let otherList = JSON.parse('[3,4,5]'); // [3, 4, 5]
-
-
-// let button = document.querySelector('button');
-// button.addEventListener('click', function (event) {
-//   let target = event.target;
-//   target.classList.toggle('button--red');
-//   target.classList.toggle('button--green');
-// });
-
-// let element = document.querySelector('select');
-// element.value; // returns '1' as that’s currently selected
-// element.value = '2';
-
-// let selectedIndex = element.selectedIndex;
-// if (selectedIndex > -1) {
-//   let selectedOption = element.querySelectorAll('option')[selectedIndex];
-// }
-
-// let ageGroupElement = document.querySelector('input[name="age-group"]:checked');
-// let value = ageGroupElement ? ageGroupElement.value : null;
-
-
-
-
-// let foodList = ['tuna', 'cheese', 'salad', 'nutter butter'];
-
-// document.write('<h3>===for LOOP===</h3>');
-// for (let i = 0; i < foodList.length; i++) {
-//   document.write('<p>' + foodList[i] + '</p>');
-// }
-
-// document.write('<h3>===forEach LOOP===</h3>');
-// foodList.forEach(function (foodItem, num) {
-//   document.write('<p>' + (num + 1) + foodItem +  '</p>');
-// });
-
-// let container = document.querySelector('.container');
-// let button = document.createElement('button');
-// button.innerText = 'Click me';
-// container.appendChild(button);
-
-// let container = document.querySelector('.container');
-// let elementToRemove = document.querySelector('p');
-// elementToRemove.parentElement.removeChild(elementToRemove);
-
-// let subtitle = document.querySelector('h3');
-// console.log(subtitle.innerText);
-// subtitle.innerText = "Hey guuuuys";
-// console.log(subtitle.innerText);
-
-// let container = document.querySelector('.container');
-// container.innerHTML = '<button>Click me</button>'
-
-// element = document.querySelector('div');
-// console.log(element.getAttribute('id'));
-// console.log(element.hasAttribute('value'));
-// element.setAttribute('id', 'new-id');
-// console.log(element.getAttribute('id'));
-
-// element.classList.contains('container');
